b42084ca7e78a638804a72285100534104eaad68
TEO OLIVER HUGHES <teo_hughes@icloud.com>
2021-06-17 19:11:43 +0100

    Initial Commit

---
+++ ExmplProject/ExmplProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExmplProjectApp.swift
+//  ExmplProject
+//
+//  Created by Hughes, Teo (BJH) on 17/06/2021.
+//
+
+import SwiftUI
+
+@main
+struct ExmplProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
